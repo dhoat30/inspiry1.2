45b1d46136f2aac0f7c5ceb67523db0f0a8ba678
Gurpreet <dhoat30@gmail.com>
2020-11-18 18:38:28 +1300

    fixed ajax-login.php

--- wp-content/themes/inspiry-theme/page-ajax-log-in.php
+++ wp-content/themes/inspiry-theme/page-ajax-log-in.php
@@ -1,16 +1,4 @@
-<?php
 
-global $wp;
-$current_url = home_url( $wp->request );
-echo $current_url;
-
-    if($current_url == 'http://localhost/inspiry/ajax-log-in'){ 
-       echo "front page";
-       echo '<h1 style="font-size: 100px">Hello</h1>';
-       echo $current_url;
-    }
-?>
-    
         <?php 
             while(have_posts()){
                 the_post(); 
