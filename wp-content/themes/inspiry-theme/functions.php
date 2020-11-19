9749db3c348078345f1bba4b580ca8b0331c6782
dhoat <dhoat30@gmail.com>
2020-11-20 10:50:28 +1300

    filter button changes

--- wp-content/themes/inspiry-theme/functions.php
+++ wp-content/themes/inspiry-theme/functions.php
@@ -27,8 +27,8 @@ require get_theme_file_path('/inc/nav-registeration.php');
     } else {
       wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/undefined'),  NULL, '1.0', true);
 
-      wp_enqueue_script('main', get_theme_file_uri('/bundled-assets/scripts.b1112a10eb77805be3da.js'), NULL, '1.0', true);
-      wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.b1112a10eb77805be3da.css'));
+      wp_enqueue_script('main', get_theme_file_uri('/bundled-assets/scripts.cc899036ed1d53d416b4.js'), NULL, '1.0', true);
+      wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.cc899036ed1d53d416b4.css'));
     }
     wp_localize_script("main", "inspiryData", array(
       "root_url" => get_site_url(),
