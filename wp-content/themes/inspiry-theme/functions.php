bb43843c1b64cf0b7fc92f24a8ec7e88074b6795
Gurpreet <dhoat30@gmail.com>
2020-11-25 19:41:26 +1300

    fixed overflow issue

--- wp-content/themes/inspiry-theme/functions.php
+++ wp-content/themes/inspiry-theme/functions.php
@@ -27,8 +27,8 @@ require get_theme_file_path('/inc/nav-registeration.php');
     } else {
       wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/undefined'),  NULL, '1.0', true);
 
-      wp_enqueue_script('main', get_theme_file_uri('/bundled-assets/scripts.cc899036ed1d53d416b4.js'), NULL, '1.0', true);
-      wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.cc899036ed1d53d416b4.css'));
+      wp_enqueue_script('main', get_theme_file_uri('/bundled-assets/scripts.5b4384b0f5f7d79e7f98.js'), NULL, '1.0', true);
+      wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.5b4384b0f5f7d79e7f98.css'));
     }
     wp_localize_script("main", "inspiryData", array(
       "root_url" => get_site_url(),
