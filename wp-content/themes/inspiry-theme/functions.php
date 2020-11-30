fc0130e8fb675f15f1919e0c563f17372de34fad
Gurpreet <dhoat30@gmail.com>
2020-11-30 17:17:49 +1300

    fixed large font padding

--- wp-content/themes/inspiry-theme/functions.php
+++ wp-content/themes/inspiry-theme/functions.php
@@ -27,8 +27,8 @@ require get_theme_file_path('/inc/nav-registeration.php');
     } else {
       wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/undefined'),  NULL, '1.0', true);
 
-      wp_enqueue_script('main', get_theme_file_uri('/bundled-assets/scripts.d6394092b68d70b27ba6.js'), NULL, '1.0', true);
-      wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.d6394092b68d70b27ba6.css'));
+      wp_enqueue_script('main', get_theme_file_uri('/bundled-assets/scripts.b86987f2a8f51e1f05d8.js'), NULL, '1.0', true);
+      wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.b86987f2a8f51e1f05d8.css'));
     }
     wp_localize_script("main", "inspiryData", array(
       "root_url" => get_site_url(),
