b9209b23c1e22d9006dbc6ce09614baa972f3c2f
Gurpreet <dhoat30@gmail.com>
2020-12-24 09:44:51 +1300

    finished on imcac

--- wp-content/themes/inspiry-theme/single-boards.php
+++ wp-content/themes/inspiry-theme/single-boards.php
@@ -3,7 +3,11 @@ get_header();
 ?>
 <div class="body-container">
     <div class="row-container board-loop-page single-board box-shadow">
-
+        <div class="back-icon-container">
+            <a href="<?php echo get_site_url(); ?>/boards">
+                <i class="fal fa-arrow-left"></i>
+            </a>
+        </div>
         <h1 class="lg-font-sz playfair-fonts regular light-grey"> <?php echo get_the_title($parentID);?></h1>
         <div class='action-btn-container'>
             <button class="share btn btn-dk-green-border font-s-regular"><i class="fal fa-share-alt"></i> Share</button>
@@ -36,10 +40,17 @@ get_header();
                         <div class="pin-options-container box-shadow">
                             <ul class="dark-grey">
                                 <li class="share-btn"><i class="fas fa-share-alt"></i> Share</li>
-                                <li class="website-btn"><a class='rm-txt-dec' target="_blank" href='<?php echo do_shortcode('[gd_post_meta key="website" id="7345" show="value-raw" no_wrap="1"]');?>'><i class="fas fa-globe"></i> Website</a></li>
+                                <!-- <li class="website-btn"><a class='rm-txt-dec' target="_blank" href='<?php// echo do_shortcode('[gd_post_meta key="website" id="7345" show="value-raw" no_wrap="1"]');?>'><i class="fas fa-globe"></i> Website</a></li>-->
                                 <li class="delete-btn" data-pinid='<?php the_ID();?>'><i class="far fa-trash-alt"></i> Delete</li>
                                 
                             </ul>
+                                <div class="share-icons box-shadow">
+                    <i class="fal fa-times"></i>
+                    <h2 class="roboto-font font-s-medium medium">Share this board</h2>
+                    <div class="underline underline-bg margin-elements"></div>
+                    <?php echo do_shortcode('[Sassy_Social_Share]');?> 
+
+                </div>
                         </div>
                         
                         <a href="<?php echo get_the_permalink(get_field('saved_project_id')); ?>">    
