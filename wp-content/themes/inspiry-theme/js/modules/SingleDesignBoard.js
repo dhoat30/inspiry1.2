3380531606d46b8f48d8a975b60c31c1c03d2b2a
Gurpreet <dhoat30@gmail.com>
2020-12-29 13:23:08 +1300

    trying to fix issues on single board

--- wp-content/themes/inspiry-theme/js/modules/SingleDesignBoard.js
+++ wp-content/themes/inspiry-theme/js/modules/SingleDesignBoard.js
@@ -1,19 +1,34 @@
-let $ = jQuery; 
+let $ = jQuery;
 
-class SingleDesignBoards { 
-    constructor(){
-        this.events(); 
+class SingleDesignBoards {
+    constructor() {
+        this.events();
     }
 
-    events(){
-        $('.action-btn-container .share').on('click', ()=>{
+    events() {
+        $('.action-btn-container .share').on('click', () => {
             $('.action-btn-container .share-icons').show();
         })
 
-        $('.action-btn-container .share-icons .fa-times').on('click', ()=>{
+        $('.action-btn-container .share-icons .fa-times').on('click', () => {
             $('.action-btn-container .share-icons').hide();
+        })
+
+        //single card share board
+        $('.single-board .board-card .share-btn').on('click', this.showCardShareContainer.bind(this));
+
+    }
+
+    showCardShareContainer(e) {
+        let container = $(e.target).closest('.dark-grey').siblings('.share-icon-container');
+        container.show();
+        $(e.target).closest('.dark-grey').siblings('.overlay').show();
+        $(e.target).closest('.dark-grey').siblings('.share-icon-container').find('.close-icon').on('click', () => {
+            container.hide();
+            $('.overlay').hide();
+
         })
     }
 }
 
-export default SingleDesignBoards; 
+export default SingleDesignBoards;
--- wp-content/themes/inspiry-theme/single-boards.php
+++ wp-content/themes/inspiry-theme/single-boards.php
@@ -15,13 +15,15 @@ get_header();
                 <i class="fal fa-times"></i>
                 <h2 class="roboto-font font-s-medium medium">Share this board</h2>
                 <div class="underline underline-bg margin-elements"></div>
-                <?php echo do_shortcode('[Sassy_Social_Share]');?> 
+                <div>
+                    <?php echo do_shortcode('[Sassy_Social_Share]');?>
+                </div>
 
             </div>
         </div>
         <div class="board-flex">
-        
-        <?php 
+
+            <?php 
             $boardLoop = new WP_Query(array(
                 'post_type' => 'boards', 
                 'post_parent' => get_the_id(),
@@ -31,54 +33,53 @@ get_header();
             while($boardLoop->have_posts()){
                 $boardLoop->the_post(); 
                 $parentID =  wp_get_post_parent_id(get_the_id()); 
-                ?>  
-                    
-                    
-                    <div class="board-card">
+                ?>
 
-                        <i class="fas fa-ellipsis-h option-icon"></i>
-                        <div class="pin-options-container box-shadow">
-                            <ul class="dark-grey">
-                                <li class="share-btn"><i class="fas fa-share-alt"></i> Share</li>
-                                <!-- <li class="website-btn"><a class='rm-txt-dec' target="_blank" href='<?php// echo do_shortcode('[gd_post_meta key="website" id="7345" show="value-raw" no_wrap="1"]');?>'><i class="fas fa-globe"></i> Website</a></li>-->
-                                <li class="delete-btn" data-pinid='<?php the_ID();?>'><i class="far fa-trash-alt"></i> Delete</li>
-                                
-                            </ul>
-                                <div class="share-icons box-shadow">
-                    <i class="fal fa-times"></i>
-                    <h2 class="roboto-font font-s-medium medium">Share this board</h2>
-                    <div class="underline underline-bg margin-elements"></div>
-                    <?php echo do_shortcode('[Sassy_Social_Share]');?> 
+
+            <div class="board-card">
+
+                <i class="fas fa-ellipsis-h option-icon"></i>
+                <div class="pin-options-container box-shadow">
+                    <ul class="dark-grey">
+                        <li class="share-btn"><i class="fas fa-share-alt"></i> Share</li>
+                        <!-- <li class="website-btn"><a class='rm-txt-dec' target="_blank" href='<?php// echo do_shortcode('[gd_post_meta key="website" id="7345" show="value-raw" no_wrap="1"]');?>'><i class="fas fa-globe"></i> Website</a></li>-->
+                        <li class="delete-btn" data-pinid='<?php the_ID();?>'><i class="far fa-trash-alt"></i> Delete
+                        </li>
+
+                    </ul>
+                    <div class="overlay"></div>
+                    <div class="share-icon-container box-shadow">
+                        <div class="roboto-font regular font-s-med"> Share this pin </div>
+                        <div class="underline"></div>
+                        <div>
+                            <?php echo do_shortcode('[Sassy_Social_Share]');?>
+                        </div>
+                        <span class="close-icon">X</span>
+                    </div>
 
                 </div>
-                        </div>
-                        
-                        <a href="<?php echo get_the_permalink(get_field('saved_project_id')); ?>">    
-                            <div class="thumbnail">
-                                <?php echo get_the_post_thumbnail( get_field('saved_project_id'), 'post-thumbnail');?>
-                            </div>
-                            <div class="title font-s-regular rm-txt-dec"><?php echo get_the_title(get_field('saved_project_id')); ?></div>
-                        </a> 
+
+                <a href="<?php echo get_the_permalink(get_field('saved_project_id')); ?>">
+                    <div class="thumbnail">
+                        <?php echo get_the_post_thumbnail( get_field('saved_project_id'), 'post-thumbnail');?>
                     </div>
-                <?php
+                    <div class="title font-s-regular rm-txt-dec">
+                        <?php echo get_the_title(get_field('saved_project_id')); ?></div>
+
+                </a>
+
+            </div>
+
+            <?php
             }
         ?>
 
-        
+
         </div>
     </div>
 </div>
 
- <div class="overlay"></div>                       
-<div class="share-icon-container box-shadow">
-                            <div class="roboto-font regular font-s-med"> Share this pin </div>
-                            <div class="underline"></div>
-                            <div>
-                                <?php echo do_shortcode('[Sassy_Social_Share  url="http:'.get_the_permalink(get_field('saved_project_id')).'"]');?>
-                            </div>
-                            <span>X</span>
 
-                        </div>
 
 <?php 
 get_footer(); 
